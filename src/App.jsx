40e7b05ff3efea9f04c1bd0d41ca9046afa5072e
ocaminov <ocaminov@gmail.com>
2024-01-16 17:22:36 -0500

    Lista ordenable con Drag and Drop

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { DndContext, closestCenter } from "@dnd-kit/core";
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+  arrayMove,
+} from "@dnd-kit/sortable";
+import User from "./User";
+
+function App() {
+  const [people, setPeople] = useState([
+    { name: "John", id: 1 },
+    { name: "Peter", id: 2 },
+    { name: "Sue", id: 3 },
+  ]);
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event;
+
+    setPeople((people) => {
+      const oldIndex = people.findIndex((person) => person.id === active.id);
+      const newIndex = people.findIndex((person) => person.id === over.id);
+      return arrayMove(people, oldIndex, newIndex);
+    });
+  };
+
+  return (
+    <div className="flex justify-center items-center">
+      <div className="w-4/6">
+        <DndContext
+          collisionDetection={closestCenter}
+          onDragEnd={handleDragEnd}
+        >
+          <h1 className="text-2xl font-bold mb-4 mt-4">Users List</h1>
+          <SortableContext
+            items={people}
+            strategy={verticalListSortingStrategy}
+          >
+            {people.map((user) => (
+              <User user={user} key={user.id} />
+            ))}
+          </SortableContext>
+        </DndContext>
+      </div>
+    </div>
+  );
+}
+
+export default App;
